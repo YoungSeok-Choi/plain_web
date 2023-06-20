6664b87b60807e240b93954f6e1f62fb9b34bebd
YoungSeok-Choi <dydtjr1717@gmail.com>
2023-06-20 16:50:13 +0900

    싸이월드 WIP

---
+++ class/03-js/01-variable.js
@@ -0,0 +1,3 @@
+
+// 자바스크립트가 임포트 되면 한번 쭉 실행시키는것 같다.
+console.log("is this execute?");
---
+++ programmers/dev_1/js/data_generator.js
@@ -0,0 +1,30 @@
+
+
+
+const fs = require("fs");
+
+
+const dataJsonList = [];
+
+for (let i = 0; i < 100; i++) {
+    dataJsonList.push({
+        id: i,
+        nickname: ""
+    });
+}
+
+
+const randStringGenerator = () => {
+
+    let chars = "";
+    for (let i = 0; i < 30; i++) {
+        const randNum = Math.floor(Math.random() * (200 - 100 + 1) + 100);
+        cahrs += String.fromCharCode;
+
+
+
+
+    }
+
+
+};
